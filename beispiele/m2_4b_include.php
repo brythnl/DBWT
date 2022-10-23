acb1ec0dde449e620835a2033abd31f51ff640ec
bryan <bryanathanaelj@gmail.com>
2022-10-24 00:31:56 +0200

    Include m2_4a file & create calls of addieren-Function

---
+++ beispiele/m2_4b_include.php
@@ -0,0 +1,7 @@
+<?php
+    include ('m2_4a_standardparameter.php');
+
+    echo '1 + 1 = ' . addieren(1, 1) . '<br>';
+    echo '2 + 2 = ' . addieren(2, 2) . '<br>';
+    echo '3 + 3 = ' . addieren(3, 3) . '<br>';
+?>
