f44f2fa1049b525a3265e88d818ab5b7bc3658d3
bryan <bryanathanaelj@gmail.com>
2022-10-24 23:05:26 +0200

    Print array as ordered list & create function to find years without winners from array

---
+++ beispiele/m2_4d_array.php
@@ -0,0 +1,53 @@
+<?php
+
+$famousMeals = [
+    1 => ['name' => 'Currywurst mit Pommes',
+          'winner' => [2001, 2003, 2007, 2010, 2020]],
+    2 => ['name' => 'Hähnchencrossies mit Paprikareis',
+          'winner' => [2002, 2004, 2008]],
+    3 => ['name' => 'Spaghetti Bolognese',
+          'winner' => [2011, 2012, 2017]],
+    4 => ['name' => 'Jägerschnitzel mit Pommes',
+          'winner' => 2019],
+];
+
+function keineGewinner($famousMeals) {
+    $totalGewinner = [];
+    for ($meal = 1; $meal <= 4; $meal++) { 
+        if (gettype($famousMeals[$meal]['winner']) == 'array') {
+            foreach($famousMeals[$meal]['winner'] as $year) {
+                $totalGewinner[] = $year;
+            }
+        } else {
+            $totalGewinner[] = $famousMeals[$meal]['winner'];
+        }
+    }
+
+    $totalYears = [];
+    for ($i = 0; $i <= 20; $i++) {
+        $totalYears[] = 2000 + $i;
+    }
+    
+    return array_diff($totalYears, $totalGewinner); // returns the years without any winners
+}
+?>
+
+<ol>
+<?php foreach ($famousMeals as $meal) {
+    echo '<li>' . $meal['name'] . '<br>';
+    if (gettype($meal['winner']) == 'array') {
+        foreach (array_reverse($meal['winner'], true) as $year) { // numeric keys preserved
+            if ($year != $meal['winner'][0]) { // index 0 is last element of reversed array
+                echo $year . ', ';
+            } else {
+                echo $year;
+            }
+        }
+    } else {
+        echo $meal['winner']; // for last meal, only one winning year
+    } 
+    echo '</li>';
+} ?>
+</ol>
+
+
