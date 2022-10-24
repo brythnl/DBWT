2da1cd6ddd4b1e3850f981950c64f075c3e457ca
bryan <bryanathanaelj@gmail.com>
2022-10-24 23:37:19 +0200

    Create Gericht-Database with an array

---
+++ werbeseite/gerichtdaten.php
@@ -0,0 +1,14 @@
+<?php
+
+$Gericht = [
+    'wok' => [ 'name' => 'Rindfleisch mit Bambus, Kaiserschoten und Rotem Paprika, dazu Mie Nudeln', 
+               'bild' => '<img src="images/wok.jpg alt="wok">'],
+    'vegetarisch' => [ 'name' => 'Spinatrisotto mit kleinen Samosateigecken und gemischter Salat', 
+                       'bild' => '<img src="images/veg.jpg alt="vegetarisch">'],
+    'klassiker' => [ 'name' => 'Schweineschnitzel mit Pommes', 
+                     'bild' => '<img src="images/klassik.jpg alt="klassiker">'],
+    'tellergericht' => [ 'name' => 'Lauch-Käse-Suppe mit Kartoffel und Rinderhack', 
+                         'bild' => '<img src="images/teller.jpg alt="tellergericht">']
+]
+
+?>
