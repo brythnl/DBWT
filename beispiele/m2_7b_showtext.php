f2fe7ef15d31f0b6bcbac40c9eff4dad18975ece
Bryan <bryanathanaelj@gmail.com>
2022-10-27 01:26:50 +0200

    create word search feature while reading file & print out translation

--- beispiele/m2_7b_showtext.php
+++ beispiele/m2_7b_showtext.php
@@ -1,15 +1,35 @@
-<?php 
 
-$file = fopen('./en.txt', 'r');
-
-if (!$file) {
-    die("Error beim Öffnen");
-}
-
-
-?>
-
-<form action="get">
+<form method="get">
     <label for="suchwort">Suchwort eingeben: </label>
     <input type="text" id="suchwort" name="suchwort">
 </form>
+
+<?php
+
+if (isset($_GET['suchwort'])) {
+    $suchwort = $_GET['suchwort'];
+
+    $found = false;
+
+    $file = fopen('./en.txt', 'r');
+    if (!$file) {
+        die("Error beim Öffnen");
+    }
+    
+    while (!feof($file)) {
+        $puffer = fgets($file);
+        if (stripos($puffer, $suchwort) !== false) {
+            $line_array = explode(";", $puffer);
+            echo $line_array[1];
+
+            $found = true;
+            break;
+        }   
+    }
+
+    if ($found === false) {
+        echo "Das gesuchte Wort " . $suchwort . " ist nicht enthalten.";
+    }
+}
+
+?>
