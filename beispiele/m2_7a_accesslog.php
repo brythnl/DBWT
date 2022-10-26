9f7798e36440058e2a582c70ab98d4fa5c111715
Bryan <bryanathanaelj@gmail.com>
2022-10-26 13:46:54 +0200

    write an access log file with date, time, browser and IP

---
+++ beispiele/m2_7a_accesslog.php
@@ -0,0 +1,17 @@
+<?php
+
+$file = fopen('.accesslog.txt', 'a');
+
+if (!$file) {
+    die("Error beim Öffnen");
+}
+
+$info = "Datum: " . date('d.m.Y') . 
+        "\nUhrzeit: " . date('H:i:sa') .
+        "\nBrowser: " . $_SERVER['HTTP_USER_AGENT'] .
+        "\nClient-IP: " . $_SERVER['REMOTE_ADDR'] . "\n\n"; 
+
+fwrite($file, $info);
+
+fclose($file);
+?>
