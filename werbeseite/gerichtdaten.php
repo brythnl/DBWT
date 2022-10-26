a0c2bd5d01f44eedd11aeb0a824e2d0c6b933687
Bryan <bryanathanaelj@gmail.com>
2022-10-26 12:16:26 +0200

    add id-Attribute to img elements

--- werbeseite/gerichtdaten.php
+++ werbeseite/gerichtdaten.php
@@ -2,13 +2,13 @@
 
 $Gericht = [
     'wok' => [ 'name' => 'Rindfleisch mit Bambus, Kaiserschoten und Rotem Paprika, dazu Mie Nudeln', 
-               'bild' => '<img src="images/wok.jpg alt="wok">'],
+               'bild' => '<img id="wok-img" src="./images/wok.jpg" alt="wok">'],
     'vegetarisch' => [ 'name' => 'Spinatrisotto mit kleinen Samosateigecken und gemischter Salat', 
-                       'bild' => '<img src="images/veg.jpg alt="vegetarisch">'],
+                       'bild' => '<img id="veg-img" src="./images/veg.jpg" alt="vegetarisch">'],
     'klassiker' => [ 'name' => 'Schweineschnitzel mit Pommes', 
-                     'bild' => '<img src="images/klassik.jpg alt="klassiker">'],
+                     'bild' => '<img id="klassik-img" src="./images/klassik.jpg" alt="klassiker">'],
     'tellergericht' => [ 'name' => 'Lauch-Käse-Suppe mit Kartoffel und Rinderhack', 
-                         'bild' => '<img src="images/teller.jpg alt="tellergericht">']
+                         'bild' => '<img id="teller-img" src="./images/teller.jpg" alt="tellergericht">']
 ]
 
 ?>
