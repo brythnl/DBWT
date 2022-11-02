005c4bb76850e04d60d9a71b638b676626e3cb87
Bryan <bryanathanaelj@gmail.com>
2022-11-02 12:29:19 +0100

    change ids of gericht images into one class

--- werbeseite/gerichtdaten.php
+++ werbeseite/gerichtdaten.php
@@ -2,13 +2,13 @@
 
 $Gericht = [
     'wok' => [ 'name' => 'Rindfleisch mit Bambus, Kaiserschoten und Rotem Paprika, dazu Mie Nudeln', 
-               'bild' => '<img id="wok-img" src="./images/wok.jpg" alt="wok">'],
+               'bild' => '<img class="gericht-img" src="./images/wok.jpg" alt="wok">'],
     'vegetarisch' => [ 'name' => 'Spinatrisotto mit kleinen Samosateigecken und gemischter Salat', 
-                       'bild' => '<img id="veg-img" src="./images/veg.jpg" alt="vegetarisch">'],
+                       'bild' => '<img class="gericht-img" src="./images/veg.jpg" alt="vegetarisch">'],
     'klassiker' => [ 'name' => 'Schweineschnitzel mit Pommes', 
-                     'bild' => '<img id="klassik-img" src="./images/klassik.jpg" alt="klassiker">'],
+                     'bild' => '<img class="gericht-img" src="./images/klassik.jpg" alt="klassiker">'],
     'tellergericht' => [ 'name' => 'Lauch-Käse-Suppe mit Kartoffel und Rinderhack', 
-                         'bild' => '<img id="teller-img" src="./images/teller.jpg" alt="tellergericht">']
+                         'bild' => '<img class="gericht-img" src="./images/teller.jpg" alt="tellergericht">']
 ]
 
 ?>
