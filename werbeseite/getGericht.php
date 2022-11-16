eda92b20c5ce7d4cb0d2f472019100a5a508a86b
Bryan <bryanathanaelj@gmail.com>
2022-11-16 13:03:35 +0100

    Add used allergens as list below Gericht table

--- werbeseite/getGericht.php
+++ werbeseite/getGericht.php
@@ -14,9 +14,12 @@ if (!$link) {
 
 $gerichtsql = "SELECT id, name, preis_intern, preis_extern FROM gericht ORDER BY name ASC LIMIT 5";
 $gerichtresult = mysqli_query($link, $gerichtsql);
-$allergensql = "SELECT code, gericht_id FROM gericht_hat_allergen";
+$gerichtallergensql = "SELECT code, gericht_id FROM gericht_hat_allergen";
+$gerichtallergenresult = mysqli_query($link, $gerichtallergensql);
+$allergensql = "SELECT code, name, typ FROM allergen";
 $allergenresult = mysqli_query($link, $allergensql);
 
+$usedAllergenCode = [];
 ?>
 
 <table>
@@ -29,22 +32,43 @@ $allergenresult = mysqli_query($link, $allergensql);
     <?php 
     while ($gerichtrow = mysqli_fetch_assoc($gerichtresult)) {
     echo '<tr>';
-        echo '<td>' . $gerichtrow['name'] . '</td>';
+        echo '<td>' . utf8_encode($gerichtrow['name']) . '</td>';
         echo '<td>' . $gerichtrow['preis_intern'] . '</td>';
         echo '<td>' . $gerichtrow['preis_extern'] . '</td>';
         echo '<td>';
-        while ($allergenrow = mysqli_fetch_assoc($allergenresult)) {
-            if ($allergenrow['gericht_id'] == $gerichtrow['id']) {
-                echo $allergenrow['code'] . ' ';
+        while ($gerichtallergenrow = mysqli_fetch_assoc($gerichtallergenresult)) {
+            if ($gerichtallergenrow['gericht_id'] == $gerichtrow['id']) {
+                echo $gerichtallergenrow['code'] . ' ';
+                if (!in_array($gerichtallergenrow['code'], $usedAllergenCode)) {
+                    $usedAllergenCode[] = $gerichtallergenrow['code'];
+                }
             }
         }
-        mysqli_data_seek($allergenresult, 0); // Set result pointer back to starting row
+        mysqli_data_seek($gerichtallergenresult, 0); // Set result pointer back to starting row
         echo '</td>'; 
     echo '</tr>';
     } ?>
 </table>
 
-<?php 
+<h3>Verwendete Allergene</h3>
+<ol>
+<?php
+    while ($allergenrow = mysqli_fetch_assoc($allergenresult)) {
+        foreach ($usedAllergenCode as $code) {
+            if ($code == $allergenrow['code']) {
+                echo '<li>' . utf8_encode($allergenrow['name']) . '</li>';
+            }                
+        }
+    }
+
+?>
+</ol>
+
+
+
+
+
+
 
 
 
