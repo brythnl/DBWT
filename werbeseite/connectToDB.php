76513c243f4d4e4c3697d03be3b1e6708387464b
Bryan <bryanathanaelj@gmail.com>
2022-11-17 17:56:06 +0100

    Add module to create connection with database

---
+++ werbeseite/connectToDB.php
@@ -0,0 +1,24 @@
+<?php 
+
+$link = mysqli_connect(
+    "127.0.0.1",
+    "root",
+    "9jan2002",
+    "emensawerbeseite",
+    );
+
+if (!$link) {
+    echo "Verbindung fehlgeschlagen: ", mysqli_connect_error();
+    exit();
+}
+
+?>
+
+
+
+
+
+
+
+
+
