f0b6b54b4d3bfd3f4961a6f1b190418b1fe5688a
Bryan <bryanathanaelj@gmail.com>
2022-11-26 21:19:24 +0100

    Reinitialize emensa Laravel project

--- emensa/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => 'localhost',  // 'localhost' or '127.0.0.1'
-    'user' => 'root',       // 
-    'password' => '9jan2002', // 
-    'database' => 'emensawerbeseite',
-    // optionally: set port below if it differs from the default 3306
-    // 'port' => 13306
-];
