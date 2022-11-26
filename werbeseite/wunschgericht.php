8c9e88c6024ea8ff393d404e19988db989bb0f29
Bryan <bryanathanaelj@gmail.com>
2022-11-26 12:39:09 +0100

    Convert date into SQL-readable format

--- werbeseite/wunschgericht.php
+++ werbeseite/wunschgericht.php
@@ -1,7 +1,11 @@
 <?php 
 
+// konvertiere zu einem SQL-lesbar Datum
+$date = strtotime($_POST['datum']);
+$date = date('Y-m-d', $date);
+
 $wunschgerichtInsertsql = "INSERT INTO wunschgericht (name, beschreibung, erstellungsdatum, erstellername, ersteller_email) 
-                            //VALUES ('$_POST[name]','$_POST[beschreibung]','$_POST[datum]','$_POST[erstellername]','$_POST[email]')";
+                            VALUES ('$_POST[name]','$_POST[beschreibung]','$date','$_POST[erstellername]','$_POST[email]')";
 mysqli_query($link, $wunschgerichtInsertsql);
 
 ?>
@@ -12,7 +16,7 @@ mysqli_query($link, $wunschgerichtInsertsql);
     <label for="beschreibung">Beschreibung:</label>
     <input type="text" name="beschreibung" id="beschreibung">
     <label for="datum">Datum:</label>
-    <input type="date" name="datum" id="datum">
+    <input type="text" name="datum" id="datum">
     <label for="erstellername">Ihr/e Name:</label>
     <input type="text" name="erstellername" id="erstellername">
     <label for="email">Ihr/e E-mail:</label>
