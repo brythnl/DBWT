aba7013ab504856e371b28b2b755efad5c51facc
Bryan <bryanathanaelj@gmail.com>
2022-11-29 03:21:37 +0100

    Create app/homepage layout

---
+++ emensa/views/appLayout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>E-Mensa</title>
+        <link rel="stylesheet" href="/css/styles.css">
+    </head>
+    <body>
+        <header>
+            @yield('header')
+        </header>
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+            @yield('footer')
+        </footer>
+    </body>
+</html>
