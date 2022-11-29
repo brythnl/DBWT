02ff03fb75f205958cb1108e0de6ed4635206dd5
Bryan <bryanathanaelj@gmail.com>
2022-11-29 17:12:01 +0100

    Bolden every 2nd list member

--- emensa/views/examples/m4_7b_kategorie.blade.php
+++ emensa/views/examples/m4_7b_kategorie.blade.php
@@ -1,9 +1,17 @@
 @extends("layout")
 
+@section("cssextra")
+    <style>
+        li:nth-child(even) {
+            font-weight: bold;
+        }
+    </style>
+@endsection
+
 @section("content")
     <ul>
     @foreach($names as $name) 
-        <li>{{ $name }}</li>
+        <li>{{ $name['name'] }}</li>
     @endforeach
     </ul>
 @endsection
