b1bce49d269b60f6257cd6713fcc8db081437b93
Bryan <bryanathanaelj@gmail.com>
2022-11-29 19:18:41 +0100

    Create layout and views for m4_7d

---
+++ emensa/views/examples/layout/m4_7d_layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>m4_7d_page_<?php echo $title; ?></title> 
+    </head>
+    <body>
+        <header>@yield('header')</header>
+        <main>@yield('main')</main>
+        <footer>@yield('footer')</footer>
+    </body>
+</html>
---
+++ emensa/views/examples/pages/m4_7d_page_1.blade.php
@@ -0,0 +1,1 @@
+@extends('examples.layout.m4_7d_layout')
