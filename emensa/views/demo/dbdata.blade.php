ef205676fd844c64f035c0ee49f0b55094992745
Bryan <bryanathanaelj@gmail.com>
2022-11-29 16:36:17 +0100

    Fix gaps in gericht list (missing names with umlauts)

--- emensa/views/demo/dbdata.blade.php
+++ emensa/views/demo/dbdata.blade.php
@@ -12,15 +12,13 @@
     <pre> {{$data['beschreibung']}}</pre>
 
 @else
-
-
     <article>
         <h1>Daten aus der Datenbank der Tabelle: Gerichte</h1>
         <p>Der Controller inkludiert das benötigte Model (gerichte.php in diesem Fall)
             und ruft die benötigte Funktion <code>db_gerichte_select_all()</code> zum Laden der Daten auf</p>
         <ul>
             @forelse($data as $a)
-                <li>{{$a['name']}}</li>
+                <li>{{utf8_encode($a['name'])}}</li>
             @empty
                 <li>Keine Daten vorhanden.</li>
             @endforelse
