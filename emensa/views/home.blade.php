cb510ec86010aecc38ca507a81f16ca85331cb57
Bryan <bryanathanaelj@gmail.com>
2022-12-10 18:24:55 +0100

    Show name of logged in user

--- emensa/views/home.blade.php
+++ emensa/views/home.blade.php
@@ -10,6 +10,7 @@
             <li><a href="#zahlen">Zahlen</a></li>
             <li><a href="#kontakt">Kontakt</a></li>
             <li><a href="#wichtig-fuer-uns">Wichtig für uns</a></li>
+            <li>Angemeldet als {{ $username }}</li>
         </ul>
     </nav>
 @endsection
