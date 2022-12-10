761982a32c676a0de74a1b22678d596bbe753989
Bryan <bryanathanaelj@gmail.com>
2022-12-10 18:17:23 +0100

    Add function to set login (success & failure) time

--- emensa/models/benutzer.php
+++ emensa/models/benutzer.php
@@ -3,15 +3,30 @@
 function incLogin($username) {
     $link = connectdb();
 
-    // Use prepared statements to bind username as condition
     $sql = mysqli_stmt_init($link);
     mysqli_stmt_prepare($sql, 
-        "UPDATE benutzer SET anzahlanmeldungen = anzahlanmeldungen + 1 WHERE name=?");
+        "UPDATE benutzer SET anzahlanmeldungen = anzahlanmeldungen + 1 WHERE name = ?");
     mysqli_stmt_bind_param($sql, 's',
         $username);
     mysqli_stmt_execute($sql);
 
     mysqli_close($link);
+}
+
+function setLoginTime($username, $error) {
+    $link = connectdb();
+
+    $date = date('Y-m-d h:i:s a', time()); // get current time
+
+    $sql = mysqli_stmt_init($link);
+    mysqli_stmt_prepare($sql,
+        "UPDATE benutzer SET " . (!$error ? "letzteanmeldung" : "letzterfehler") . " = $date WHERE name = ?");
+    mysqli_stmt_bind_param($sql, 's',
+        $username);
+    mysqli_stmt_execute($sql);
+
+    mysqli_close($link);
+        
 }
 
 ?>
