870234899d75bb81a41788acbc3371287ab31285
Bryan <bryanathanaelj@gmail.com>
2022-12-14 00:00:26 +0100

    Add log info for login and warning for unsuccessful login

--- emensa/controllers/AuthController.php
+++ emensa/controllers/AuthController.php
@@ -6,19 +6,23 @@ class AuthController {
         return view('login', ['msg' => $msg]);
     }
 
-    public function check(RequestData $rd) {
+    /*public function check(RequestData $rd) {
         $username = $rd->query['username'] ?? false;
         $password = $rd->query['password'] ?? false;
+        
+        $logger = logger();
 
         $_SESSION['login_result_message'] = NULL;
-        if () {
+        if () { // HOW TO DETERMINE IF LOGIN IS SUCCESSFULL
+            $logger->info('Erfolgreich angemeldet!');
             $_SESSION['login_ok'] = true;
             header('Location: /');
         } else {
+            $logger->warn('Bitte die korrekten Anmeldedaten eintragen!')
             $_SESSION['login_result_message'] = 'Falscher Name oder Passwort! Bitte erneut eintragen.';
             header('Location: /anmeldung');
         }
-    }
+    }*/
 }
 
 ?>
