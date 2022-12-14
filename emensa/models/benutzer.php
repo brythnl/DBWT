927741b29f92a59d3a8f83b9e9932a86476687d3
Bryan <bryanathanaelj@gmail.com>
2022-12-14 21:14:45 +0100

    Fix update letzteanmeldung/fehler

--- emensa/models/benutzer.php
+++ emensa/models/benutzer.php
@@ -16,11 +16,12 @@ function incLogin($username) {
 function setLoginTime($username, $error) {
     $link = connectdb();
 
-    $date = date('Y-m-d h:i:s a', time()); // get current time
+    date_default_timezone_set('Europe/Berlin');
+    $date = date("Y-m-d H:i:s"); // get current time
 
     $sql = mysqli_stmt_init($link);
     mysqli_stmt_prepare($sql,
-        "UPDATE benutzer SET " . (!$error ? "letzteanmeldung" : "letzterfehler") . " = $date WHERE email = ?");
+        "UPDATE benutzer SET " . (!$error ? "letzteanmeldung" : "letzterfehler") . " = '$date' WHERE email = ?");
     mysqli_stmt_bind_param($sql, 's',
         $username);
     mysqli_stmt_execute($sql);
