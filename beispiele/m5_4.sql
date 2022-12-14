981b05f50c1f3bb011c93aba21320bdda43b4da3
Bryan <bryanathanaelj@gmail.com>
2022-12-14 21:18:49 +0100

    Add SQL views

---
+++ beispiele/m5_4.sql
@@ -0,0 +1,57 @@
+/* CREATE VIEW Syntax
+CREATE VIEW view_name AS
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition; */
+USE emensawerbeseite;
+
+CREATE VIEW view_suppengerichte AS
+SELECT name
+FROM gericht
+WHERE name LIKE '%suppe%';
+
+CREATE VIEW view_anmeldungen AS
+SELECT anzahlanmeldungen
+FROM benutzer;
+SELECT * FROM view_anmeldungen
+ORDER BY anzahlanmeldungen DESC;
+
+CREATE VIEW view_kategoriegerichte_vegetarisch AS
+SELECT gericht.name, kategorie.name AS kategorie_name
+FROM gericht
+RIGHT JOIN gericht_hat_kategorie ON gericht.id = gericht_hat_kategorie.gericht_id
+RIGHT JOIN kategorie ON gericht_hat_kategorie.kategorie_id = kategorie.id
+WHERE gericht.name is null OR vegetarisch=1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
