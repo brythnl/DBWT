17c297acaa2820a64e44e3815691660b85f9c42e
Bryan <bryanathanaelj@gmail.com>
2022-12-14 19:47:23 +0100

    Add login check logic

--- emensa/controllers/AuthController.php
+++ emensa/controllers/AuthController.php
@@ -1,4 +1,6 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/../models/benutzer.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/../../beispiele/passwort.php');
 
 class AuthController {
     public function index(RequestData $rd) {
@@ -6,23 +8,42 @@ class AuthController {
         return view('login', ['msg' => $msg]);
     }
 
-    /*public function check(RequestData $rd) {
-        $username = $rd->query['username'] ?? false;
-        $password = $rd->query['password'] ?? false;
-        
+    public function check(RequestData $rd) {
         $logger = logger();
 
+        $username = $rd->getPostData()['username'] ?? false;
+        $password = $rd->getPostData()['password'] ?? false;
+  
+        $success = false;
+        $exist = false;
+        $logindata = getLoginData();
+        foreach ($logindata as $user) {
+          if ($user['email'] == $username) {
+            $exist = true;
+            if ($user['passwort'] == hashPassword($password)) {
+              $success = true;
+            }
+          }
+        }
+
         $_SESSION['login_result_message'] = NULL;
-        if () { // HOW TO DETERMINE IF LOGIN IS SUCCESSFULL
+        if ($success) {
             $logger->info('Erfolgreich angemeldet!');
             $_SESSION['login_ok'] = true;
+            incLogin($username);
+            setLoginTime($username, false);
+            $_SESSION['username'] = $username;
             header('Location: /');
         } else {
-            $logger->warn('Bitte die korrekten Anmeldedaten eintragen!')
+            $logger->warning('Bitte die korrekten Anmeldedaten eintragen!');
             $_SESSION['login_result_message'] = 'Falscher Name oder Passwort! Bitte erneut eintragen.';
+            if ($exist) {
+              setLoginTime($username, true); // User existiert aber falsches passwort
+            }
             header('Location: /anmeldung');
         }
-    }*/
+    }
 }
+
 
 ?>
