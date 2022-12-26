2149b26d8b3a505627cbb757d55cb073e4ef7ee3
Bryan <bryanathanaelj@gmail.com>
2022-12-26 13:37:47 +0100

    Use request class to use request

--- emensamobil/app/Http/Controllers/HomeController.php
+++ emensamobil/app/Http/Controllers/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-class HomeController extends Controller 
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+class HomeController extends BaseController 
 {
-    public function index(RequestData $request) {
+    public function index(Request $request) {
       $gerichte = db_gericht_select_all();
       $logger = logger();
       $logger->info('Hauptseite aufgeruft!');
