79dc117e45c0527f7852132db10fd164ad021207
Bryan <bryanathanaelj@gmail.com>
2022-12-28 16:51:00 +0100

    Create Blade homepage view

---
+++ emensamobil/resources/views/home.blade.php
@@ -0,0 +1,83 @@
+@extends("layouts.home")
+
+@section('header')
+    <nav>
+        <img src="/img/logo.png" id="logo" alt="logo">
+        <h1>e-mensa</h1>
+        <ul class="navbar">
+            <li><a href="#ankundigung">Ankündigung</a></li>
+            <li><a href="#speisen">Speisen</a></li>
+            <li><a href="#zahlen">Zahlen</a></li>
+            <li><a href="#kontakt">Kontakt</a></li>
+            <li><a href="#wichtig-fuer-uns">Wichtig für uns</a></li>
+        </ul>
+    </nav>
+@endsection
+
+@section('content')
+    <div id="img-container">
+        <img src="/img/mensa.jpg" id="mensa" alt="mensa">
+    </div>
+
+    <article id="ankundigung">
+        <h2>Bald gibt es Essen auch online ;)</h2>
+        <div id="ankundigungsbox">  
+            <p>Lorem ipsum dolor sit amet, risus viverra adipiscing at in. Nec feugiat nisl pretium fusce id. A iaculis at erat pellentesque. Vel facilisis volutpat est velit. Porttitor massa id neque aliquam. Ullamcorper morbi tincidunt ornare massa eget egestas purus. Ullamcorper eget nulla facilisi etiam dignissim. Eleifend donec pretium vulputate sapien nec sagittis. Elit sed vulputate mi sit.</p>
+            <p>A cras semper auctor neque vitae tempus quam pellentesque nec. Interdum varius sit amet mattis vulputate enim nulla. Non odio euismod lacinia at quis risus. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus et. Orci porta non pulvinar neque laoreet suspendisse interdum consectetur libero.</p>
+        </div>
+    </article>
+
+    <section id="speisen">
+        <h2>Köstlichkeiten, die Sie erwarten</h2>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Interner Preis</th>
+                <th>Externer Preis</th>
+            </tr>
+            @foreach ($gerichte as $gericht) 
+                <tr>
+                    <td>{{ utf8_encode($gericht['name']) }}</td>
+                    <td>{{ $gericht['preis_intern'] }}</td>
+                    <td>{{ $gericht['preis_extern'] }}</td>
+                    <td><img class="gericht-img"
+                        @if ($gericht['bildname'] == NULL) 
+                            src="/img/gerichte/00_image_missing.jpg"
+                        @else
+                            src={{ "/img/gerichte/" . $gericht['bildname'] }}
+                        @endif
+                    ></td>
+                </tr>
+            @endforeach
+        </table>
+    </section>
+    
+    <section id="wichtig">
+        <h2 id="wichtig-fuer-uns">Das ist uns wichtig</h2>
+        <ul id="wichtig-list">
+            <li>Beste frische saisonale Zutaten</li>
+            <li>Ausgewogene abwechslungsreiche Gerichte</li>
+            <li>Sauberkeit</li>
+        </ul>
+    </section>
+
+    <h2 id="closing">Wir freuen uns auf Ihren Besuch!</h2>
+@endsection
+
+@section('footer')
+    <ul id="closing-list">
+        <li>(c) E-Mensa GmbH</li>
+        <li>Bryan Nathanael Joestin, Alexander Matthew</li>
+        <li><a>Impressum</a></li>
+    </ul>
+@endsection
+
+@section("cssextra")
+    <link rel="stylesheet" href="/css/default.min.css">
+    <style>
+    </style>
+@endsection
+
+@section("jsextra")
+   <script src="/js/highlight.min.js"></script><script>hljs.highlightAll();</script>
+@endsection
