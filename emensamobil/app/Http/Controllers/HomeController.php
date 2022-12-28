458f7cd8f5f79a3cdb52995566864fad9c57d0be
Bryan <bryanathanaelj@gmail.com>
2022-12-28 16:56:07 +0100

    Add model query for all data from gericht table

--- emensamobil/app/Http/Controllers/HomeController.php
+++ emensamobil/app/Http/Controllers/HomeController.php
@@ -5,15 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 
+use App\Models\Gericht;
+
 class HomeController extends BaseController 
 {
-    public function index(Request $request) {
-      $gerichte = db_gericht_select_all();
-      $logger = logger();
-      $logger->info('Hauptseite aufgeruft!');
-        return view('home', [
-            'rd' => $request,
-            'gerichte'=>$gerichte 
-        ]);
+  public function index(Request $request) {
+    $gerichte = Gericht::all();
+    return view('home', [
+      'rd' => $request,
+      'gerichte'=>$gerichte 
+    ]);
     }
 }
