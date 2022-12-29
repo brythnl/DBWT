2b3c0fe6eb5b8aaea8df079eb065b83ed55cf3eb
Bryan <bryanathanaelj@gmail.com>
2022-12-29 02:56:20 +0100

    Add route to login page

--- emensamobil/routes/web.php
+++ emensamobil/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,4 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'index']);
-
+Route::get('/login', [AuthController::class, 'index']);
