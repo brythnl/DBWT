bacfdcf22db1366b039c4fec8a2ea2532ee44570
Bryan <bryanathanaelj@gmail.com>
2022-12-29 15:03:59 +0100

    Add form action & method, minlength for textarea & submit button

--- emensa/views/rating.blade.php
+++ emensa/views/rating.blade.php
@@ -1,9 +1,9 @@
 @extends('appLayout')
 
 @section('content')
-  <form>
+  <form action="/bewertung_speichern?gerichtid={{ $gerichtid }}" method="post">
     <label for="bemerkung">Bemerkungen:</label>
-    <textarea id="bemerkung" name="bemerkung" maxlength="800"></textarea>
+    <textarea id="bemerkung" name="bemerkung" minlength="5" maxlength="800"></textarea>
     <label for="sterne">Sterne:</label>
     <select id="sterne" name="sterne">
       <option value="sehr gut">sehr gut</option>
@@ -11,5 +11,7 @@
       <option value="schlecht">schlecht</option>
       <option value="sehr schlecht">sehr schlecht</option>
     </select>
+    <input type="submit" value="Submit">
   </form>
+
 @endsection
