380e2f6c3067934befda1da1ce5a39f3d125e449
Bryan <bryanathanaelj@gmail.com>
2022-12-29 15:03:13 +0100

    Add function to save form data

--- emensa/controllers/RatingController.php
+++ emensa/controllers/RatingController.php
@@ -1,14 +1,24 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/../models/bewertung.php');
 
 class RatingController {
   public function index(RequestData $request) {
     $_SESSION['access_rating'] = true;
     if ($_SESSION['login_ok']) {
       $gerichtid = $request->query['gerichtid'];
-      return view('rating');
+      return view('rating', ['gerichtid'=>$gerichtid]);
     } else {
       header('Location: /anmeldung');
     }
+  }
+
+  public function save(RequestData $request) {
+    $gerichtid = $request->query['gerichtid'];
+    $bemerkung = $request->getPostData()['bemerkung'];
+    $sterne = $request->getPostData()['sterne'];
+    saveData($gerichtid, $bemerkung, $sterne);
+
+    header('Location: /bewertung?gerichtid=' . $gerichtid);
   }
 
 }
