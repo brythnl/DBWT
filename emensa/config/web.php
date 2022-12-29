056c196cd1600976afe3e97dab24082065bc8f04
Bryan <bryanathanaelj@gmail.com>
2022-12-29 13:46:08 +0100

    Add route to rating form page

--- emensa/config/web.php
+++ emensa/config/web.php
@@ -7,21 +7,23 @@
  */
 
 return array(
-    '/'             => "HomeController@index",
-    "/demo"         => "DemoController@demo",
-    '/dbconnect'    => 'DemoController@dbconnect',
-    '/debug'        => 'HomeController@debug',
-    '/error'        => 'DemoController@error',
-    '/requestdata'   => 'DemoController@requestdata',
+  '/'             => "HomeController@index",
+  "/demo"         => "DemoController@demo",
+  '/dbconnect'    => 'DemoController@dbconnect',
+  '/debug'        => 'HomeController@debug',
+  '/error'        => 'DemoController@error',
+  '/requestdata'   => 'DemoController@requestdata',
 
-    // Routes for Aufgabe 7
-    '/m4_7a_queryparameter' => 'ExampleController@m4_7a_queryparameter',
-    '/m4_7b_kategorie' => 'ExampleController@m4_7b_kategorie',
-    '/m4_7c_gerichte' => 'ExampleController@m4_7c_gerichte',
-    '/m4_7d_layout' => 'ExampleController@m4_7d_layout',
+  // Routes for Aufgabe 7
+  '/m4_7a_queryparameter' => 'ExampleController@m4_7a_queryparameter',
+  '/m4_7b_kategorie' => 'ExampleController@m4_7b_kategorie',
+  '/m4_7c_gerichte' => 'ExampleController@m4_7c_gerichte',
+  '/m4_7d_layout' => 'ExampleController@m4_7d_layout',
 
-    // Log-in route
-    '/anmeldung' => 'AuthController@index',
-    '/anmeldung_verifizieren' => 'AuthController@check',
-    '/abmeldung' => 'AuthController@logout',
+  // Log-in route
+  '/anmeldung' => 'AuthController@index',
+  '/anmeldung_verifizieren' => 'AuthController@check',
+  '/abmeldung' => 'AuthController@logout',
+  '/bewertung' => 'RatingController@index',
+
 );
