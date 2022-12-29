1fb5fe1d8e0a62fd0a466362657ea3253b4a290c
Bryan <bryanathanaelj@gmail.com>
2022-12-29 02:56:38 +0100

    Add view to login page

---
+++ emensamobil/resources/views/login.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.home")
+
+@section('content')
+    <p>{{ $msg }}</p>
+    <form method="post" action="/anmeldung_verifizieren">
+        <label for="username">Enter a username:</label>
+        <input type="text" id="username" name="username">
+        <label for="password">Enter a password:</label>
+        <input type="password" id="password" name="password">
+        <input type="submit" value="Anmeldung">
+    </form>
+@endsection
