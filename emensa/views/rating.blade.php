a15e4f2177a32c8702f59ee27518b8c3a99f8518
Bryan <bryanathanaelj@gmail.com>
2022-12-29 13:53:29 +0100

    Add view for rating form page

---
+++ emensa/views/rating.blade.php
@@ -0,0 +1,15 @@
+@extends('appLayout')
+
+@section('content')
+  <form>
+    <label for="bemerkung">Bemerkungen:</label>
+    <textarea id="bemerkung" name="bemerkung"></textarea>
+    <label for="sterne">Sterne:</label>
+    <select id="sterne" name="sterne">
+      <option value="sehr gut">sehr gut</option>
+      <option value="gut">gut</option>
+      <option value="schlecht">schlecht</option>
+      <option value="sehr schlecht">sehr schlecht</option>
+    </select>
+  </form>
+@endsection
