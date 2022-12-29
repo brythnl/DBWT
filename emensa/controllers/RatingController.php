b9bc225ef2240d3264014738792290b679e8c6ca
Bryan <bryanathanaelj@gmail.com>
2022-12-29 21:26:11 +0100

    Pass in 30 ratings data into rating view

--- emensa/controllers/RatingController.php
+++ emensa/controllers/RatingController.php
@@ -5,25 +5,28 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/../models/gericht.php');
 class RatingController {
   public function index(RequestData $request) {
     $_SESSION['access_rating'] = true;
+    $gerichtid = $request->query['gerichtid'];
     if ($_SESSION['login_ok']) {
-      $gerichtid = $request->query['gerichtid'];
       $gericht = getNameImg($gerichtid);
+      $ratings = get_30_ratings_chronological();
       return view('rating', [
         'gerichtid'=>$gerichtid,
         'gericht'=>$gericht,
+        'ratings'=>$ratings,
       ]);
     } else {
-      header('Location: /anmeldung');
+      header('Location: /anmeldung?gerichtid=' . $gerichtid);
     }
   }
 
   public function save(RequestData $request) {
     $gerichtid = $request->query['gerichtid'];
+    $gericht = getNameImg($gerichtid);
+
     $bemerkung = $request->getPostData()['bemerkung'];
     $sterne = $request->getPostData()['sterne'];
-    saveData($gerichtid, $bemerkung, $sterne);
+    saveData($gerichtid, $bemerkung, $sterne, $gericht['name']);
 
     header('Location: /bewertung?gerichtid=' . $gerichtid);
   }
-
 }
