e88b8f1cb86b57756335b98e2aeff8a0ca6cfb5c
Bryan <bryanathanaelj@gmail.com>
2022-12-29 14:24:19 +0100

    Set variable to get gerichtid query param

--- emensa/controllers/RatingController.php
+++ emensa/controllers/RatingController.php
@@ -4,6 +4,7 @@ class RatingController {
   public function index(RequestData $request) {
     $_SESSION['access_rating'] = true;
     if ($_SESSION['login_ok']) {
+      $gerichtid = $request->query['gerichtid'];
       return view('rating');
     } else {
       header('Location: /anmeldung');
