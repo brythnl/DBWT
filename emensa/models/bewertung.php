21db7c483324825b6a97f29524705bd1d77dc9ca
Bryan <bryanathanaelj@gmail.com>
2022-12-29 21:26:49 +0100

    Add function to get 30 ratings from database

--- emensa/models/bewertung.php
+++ emensa/models/bewertung.php
@@ -1,17 +1,30 @@
 <?php
 
-function saveData($gerichtid, $bemerkung, $sterne) {
+function saveData($gerichtid, $bemerkung, $sterne, $name) {
   $link = connectdb();
 
   $sql = mysqli_stmt_init($link);
   mysqli_stmt_prepare($sql,
-    "INSERT INTO bewertung (gericht_id, bemerkung, sterne) VALUES (?, ?, ?)");
-  mysqli_stmt_bind_param($sql, 'iss',
+    "INSERT INTO bewertung (gericht_id, bemerkung, sterne, name) VALUES (?, ?, ?, ?)");
+  mysqli_stmt_bind_param($sql, 'isss',
     $gerichtid,
     $bemerkung,
-    $sterne
+    $sterne,
+    $name
   );
   mysqli_stmt_execute($sql);
 
   mysqli_close($link);
+}
+
+function get_30_ratings_chronological() {
+  $link = connectdb();
+
+  $sql = "SELECT * FROM bewertung ORDER BY zeitpunkt DESC LIMIT 30";
+  $result = mysqli_query($link, $sql);
+  $data = mysqli_fetch_all($result, MYSQLI_BOTH);
+
+  mysqli_close($link);
+
+  return $data;
 }
