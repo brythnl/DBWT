5f7d33622785fa7041f4e844e85e712b88305d51
Bryan <bryanathanaelj@gmail.com>
2023-01-01 17:26:40 +0100

    Remove access_rating session variable

--- emensa/controllers/RatingController.php
+++ emensa/controllers/RatingController.php
@@ -3,45 +3,46 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/../models/bewertung.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/../models/gericht.php');
 
 class RatingController {
-  public function index(RequestData $request) {
-    $_SESSION['access_rating'] = true;
-    $gerichtid = $request->query['gerichtid'];
-    if ($_SESSION['login_ok']) {
-      $gericht = getNameImg($gerichtid);
-      $ratings = get_30_ratings_chronological();
-      return view('rating', [
-        'gerichtid'=>$gerichtid,
-        'gericht'=>$gericht,
-        'ratings'=>$ratings,
-      ]);
-    } else {
-      header('Location: /anmeldung?gerichtid=' . $gerichtid);
+    public function index(RequestData $request) {
+        $gerichtid = $request->query['gerichtid'];
+        if ($_SESSION['login_ok']) {
+            $gericht = getNameImg($gerichtid);
+            $ratings = get_30_ratings_chronological();
+            $admin = checkAdmin($_SESSION['username']);
+            return view('rating', [
+                'gerichtid'=>$gerichtid,
+                'gericht'=>$gericht,
+                'ratings'=>$ratings,
+                'admin'=>$admin,
+            ]);
+        } else {
+            header('Location: /anmeldung?gerichtid=' . $gerichtid);
+        }
     }
-  }
 
-  public function save(RequestData $request) {
-    $gerichtid = $request->query['gerichtid'];
-    $gericht = getNameImg($gerichtid);
+    public function save(RequestData $request) {
+        $gerichtid = $request->query['gerichtid'];
+        $gericht = getNameImg($gerichtid);
 
-    $bemerkung = $request->getPostData()['bemerkung'];
-    $sterne = $request->getPostData()['sterne'];
-    saveData($gerichtid, $bemerkung, $sterne, $gericht['name'], $_SESSION['username']);
+        $bemerkung = $request->getPostData()['bemerkung'];
+        $sterne = $request->getPostData()['sterne'];
+        saveData($gerichtid, $bemerkung, $sterne, $gericht['name'], $_SESSION['username']);
 
-    header('Location: /bewertung?gerichtid=' . $gerichtid);
-  }
-
-  public function userRating() {
-    if ($_SESSION['login_ok']) {
-      $ratings = get_user_ratings_chronological($_SESSION['username']);
-      return view('userRating', [
-        'ratings'=>$ratings,
-      ]);
+        header('Location: /bewertung?gerichtid=' . $gerichtid);
     }
-  }
 
-  public function delete(RequestData $request) {
-    delete_user_rating($request->query['gerichtid'], $request->query['autor']);
+    public function userRating() {
+        if ($_SESSION['login_ok']) {
+            $ratings = get_user_ratings_chronological($_SESSION['username']);
+            return view('userRating', [
+                'ratings'=>$ratings,
+            ]);
+        }
+    }
 
-    header('Location: /meinebewertung');
-  }
+    public function delete(RequestData $request) {
+        delete_user_rating($request->query['gerichtid'], $request->query['autor']);
+
+        header('Location: /meinebewertung');
+    }
 }
