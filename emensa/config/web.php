d717133d2c506aa5f41f590f449c9235814eb913
Bryan <bryanathanaelj@gmail.com>
2023-01-01 17:57:09 +0100

    Add route for Hervorhebung

--- emensa/config/web.php
+++ emensa/config/web.php
@@ -7,27 +7,28 @@
  */
 
 return array(
-  '/'             => "HomeController@index",
-  "/demo"         => "DemoController@demo",
-  '/dbconnect'    => 'DemoController@dbconnect',
-  '/debug'        => 'HomeController@debug',
-  '/error'        => 'DemoController@error',
-  '/requestdata'   => 'DemoController@requestdata',
+    '/'             => "HomeController@index",
+    "/demo"         => "DemoController@demo",
+    '/dbconnect'    => 'DemoController@dbconnect',
+    '/debug'        => 'HomeController@debug',
+    '/error'        => 'DemoController@error',
+    '/requestdata'   => 'DemoController@requestdata',
 
-  // Routes for Aufgabe 7
-  '/m4_7a_queryparameter' => 'ExampleController@m4_7a_queryparameter',
-  '/m4_7b_kategorie' => 'ExampleController@m4_7b_kategorie',
-  '/m4_7c_gerichte' => 'ExampleController@m4_7c_gerichte',
-  '/m4_7d_layout' => 'ExampleController@m4_7d_layout',
+    // Routes for Aufgabe 7
+    '/m4_7a_queryparameter' => 'ExampleController@m4_7a_queryparameter',
+    '/m4_7b_kategorie' => 'ExampleController@m4_7b_kategorie',
+    '/m4_7c_gerichte' => 'ExampleController@m4_7c_gerichte',
+    '/m4_7d_layout' => 'ExampleController@m4_7d_layout',
 
-  // Log-in route
-  '/anmeldung' => 'AuthController@index',
-  '/anmeldung_verifizieren' => 'AuthController@check',
-  '/abmeldung' => 'AuthController@logout',
+    // Log-in routes
+    '/anmeldung' => 'AuthController@index',
+    '/anmeldung_verifizieren' => 'AuthController@check',
+    '/abmeldung' => 'AuthController@logout',
 
-  // Rating route
-  '/bewertung' => 'RatingController@index',
-  '/bewertung_speichern' => 'RatingController@save',
-  '/meinebewertung' => 'RatingController@userRating',
-  '/bewertung_loeschen' => 'RatingController@delete',
+    // Rating routes
+    '/bewertung' => 'RatingController@index',
+    '/bewertung_speichern' => 'RatingController@save',
+    '/meinebewertung' => 'RatingController@userRating',
+    '/bewertung_loeschen' => 'RatingController@delete',
+    '/hervorheben' => 'RatingController@setSelection',
 );
