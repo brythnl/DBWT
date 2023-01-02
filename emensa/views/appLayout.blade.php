c52cb55e8d78b5a1118e02c438ca25cd825ca5b6
Bryan <bryanathanaelj@gmail.com>
2023-01-02 22:29:43 +0100

    Add meta tag for viewport

--- emensa/views/appLayout.blade.php
+++ emensa/views/appLayout.blade.php
@@ -2,6 +2,7 @@
 <html lang="de">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>E-Mensa @yield('title')</title>
         <link rel="stylesheet" href="/css/appStyles.css">
     </head>
