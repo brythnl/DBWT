3b1a763a7155198daaad053e2c2c616472234ba5
Bryan <bryanathanaelj@gmail.com>
2023-01-02 14:00:22 +0100

    Pass in hervorgehobene ratings into homepage view

--- emensa/controllers/HomeController.php
+++ emensa/controllers/HomeController.php
@@ -1,17 +1,20 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'].'/../models/gericht.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/../models/benutzer.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/../models/bewertung.php');
 
 
 /* Datei: controllers/HomeController.php */
 class HomeController {
     public function index(RequestData $request) {
-      $gerichte = db_gericht_select_all();
-      $logger = logger();
-      $logger->info('Hauptseite aufgeruft!');
+        $gerichte = db_gericht_select_all();
+        $ratings = get_selected_ratings();
+        $logger = logger();
+        $logger->info('Hauptseite aufgeruft!');
         return view('home', [
             'rd' => $request,
-            'gerichte'=>$gerichte 
+            'gerichte'=>$gerichte,
+            'ratings'=>$ratings,
         ]);
     }
     
