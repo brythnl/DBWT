997afdc1d63f2b931018a82774fbe867b7842e74
Bryan <bryanathanaelj@gmail.com>
2023-01-02 14:03:20 +0100

    Require orm config file

--- emensa/public/index.php
+++ emensa/public/index.php
@@ -16,6 +16,8 @@ try {
     // file exists
     require_once realpath($_SERVER['DOCUMENT_ROOT'] . "/../vendor/autoload.php");
 
+    require_once "../config/orm.php";
+
 } catch (Exception $ex) {
     echo "<code>DOCUMENT_ROOT</code><br><pre>{$_SERVER['DOCUMENT_ROOT']}</pre><code>Error</code><br><pre>" . $ex->getMessage() . "</pre>";
 }
